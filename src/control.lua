7848a03acf674d9121fc25ce76d653afd0ff8aed
Guy Fraser <guy.fraser1@gmail.com>
2016-09-18 10:01:38 +0100

    Some suggestions
    * Converted to use local variable
    * Config stored on global to persist save/load cycle
    * Refactored entity_id() function

--- src/control.lua
+++ src/control.lua
@@ -1,56 +1,70 @@
+-- luacheck: globals game script defines global
+
 -- config loading
-if not dtms then dtms = {} end
-if not dtms.config then dtms.config = {debug_logging=false} end
+global.dtms = global.dtms or {}
+global.dtms.config = global.dtms.config or {}
+
 require("config")
 
 -- global state
-if not dtms.locked_entity_ids then dtms.locked_entity_ids = {} end -- the player's locked entities
+if not global.dtms.locked_entity_ids then global.dtms.locked_entity_ids = {} end -- the player's locked entities
 
-function log(str)
-    if dtms.config and dtms.config.debug_logging then
+local function log(str)
+    if global.dtms.config and global.dtms.config.debug_logging then
 	    game.players[1].print( "[" .. game.tick .. "] " .. tostring(str))
-    end
-end
-
-function lock_entity(entity)
-    -- log("Entity Locked: " .. entity_id(entity) .. " (" .. entity.name .. ")")
-    dtms.locked_entity_ids[entity_id(entity)] = true
-
-    if dtms.config and not dtms.config.allow_rotate then entity.minable = false end
-    if dtms.config and not dtms.config.allow_mining then entity.rotatable = false end
-    if dtms.config and not dtms.config.allow_shooting then entity.destructible = false end
-end
-
-function unlock_entity(entity)
-    if(dtms.locked_entity_ids[entity_id(entity)]) then
-        log("Entity Unlocked: " .. entity_id(entity) .. " (" .. entity.name .. ")")
-        if dtms.config and not dtms.config.allow_rotate then entity.minable = true end
-        if dtms.config and not dtms.config.allow_mining then entity.rotatable = true end
-        if dtms.config and not dtms.config.allow_shooting then entity.destructible = true end
-        dtms.locked_entity_ids[entity_id(entity)] = nil
     end
 end
 
 -- Return a string representing the unique entity id.
 -- https://forums.factorio.com/viewtopic.php?f=25&t=32860&sid=a116fe079b1b058b91d496056a1b4fbe
-function entity_id(entity)
-    if not entity or not entity.valid then return nil end
-    return entity.name .. "-" .. entity.position.x .. "-" .. entity.position.y .. "-" .. entity.surface.name .. "-" .. entity.force.name
+local function entity_id(entity)
+    -- note: we return 'ignore' if not able to supply decent entity id
+    -- this avoids calling code breaking if using the id as table key
+    -- might lead to some minor cruft, but nothing worth worrying about
+    if not entity or not entity.valid or not entity.unit_number then return 'ignore' end
+    return entity.unit_number
 end
 
-function evaluate_callbacks()
+local function lock_entity(entity)
+    -- TODO: check entity isn't already locked
+    --       when locking record current state, so that it can be restored properly later
+
+    -- log("Entity Locked: " .. entity_id(entity) .. " (" .. entity.name .. ")")
+    global.dtms.locked_entity_ids[entity_id(entity)] = true
+
+    if global.dtms.config then
+        if not global.dtms.config.allow_rotate then entity.minable = false end
+        if not global.dtms.config.allow_mining then entity.rotatable = false end
+        if not global.dtms.config.allow_shooting then entity.destructible = false end
+    end
+end
+
+local function unlock_entity(entity)
+    -- TODO: check entity isn't already unlocked
+    --       when unlocking, restore to previous state rather than hard-coded values
+
+    if(global.dtms.locked_entity_ids[entity_id(entity)]) then
+        log("Entity Unlocked: " .. entity_id(entity) .. " (" .. entity.name .. ")")
+        if global.dtms.config and not global.dtms.config.allow_rotate then entity.minable = true end
+        if global.dtms.config and not global.dtms.config.allow_mining then entity.rotatable = true end
+        if global.dtms.config and not global.dtms.config.allow_shooting then entity.destructible = true end
+        global.dtms.locked_entity_ids[entity_id(entity)] = nil
+    end
+end
+
+local function evaluate_callbacks()
     if game.tick % 30 > 0 then return false end
-    dtms.Timeout.evaluateCallbacks()
+    global.dtms.Timeout.evaluateCallbacks()
 end
 
 -- unlock the entity
-function attempt_unlock(player, locked_entity)
+local function attempt_unlock(player, locked_entity)
     local selected = player.selected
     if selected then
         local locked_entity_id = entity_id(locked_entity)
         if(entity_id(locked_entity) == entity_id(selected)) then
             -- still selecting the entity, enque again
-            dtms.Timeout.setTimeout(attempt_unlock, {player, locked_entity}, 5, locked_entity_id)
+            global.dtms.Timeout.setTimeout(attempt_unlock, {player, locked_entity}, 5, locked_entity_id)
             return false
         end
     end
@@ -59,12 +73,11 @@ function attempt_unlock(player, locked_entity)
     unlock_entity(locked_entity)
 end
 
-function player_is_entity_owner(player, entity)
-    local last_user = entity.last_user
-    if last_user and last_user.index ~= player.index then return false else return true end
+local function player_is_entity_owner(player, entity)
+    return player.index == ( entity.last_user or player.index )
 end
 
-function mine_check(player)
+local function mine_check(player)
     local selected = player.selected
     if selected then
         if player_is_entity_owner(player, selected) then
@@ -76,19 +89,19 @@ function mine_check(player)
 
             -- attempt to unlock the entity in the future (when the entity is no longer selected)
             local locked_entity_id = entity_id(selected)
-            dtms.Timeout.setTimeout(attempt_unlock, {player, selected}, 5, locked_entity_id)
+            global.dtms.Timeout.setTimeout(attempt_unlock, {player, selected}, 5, locked_entity_id)
         end
     end
 end
 
 script.on_event(defines.events.on_trigger_created_entity, function(event)
-    log(entity.name)
+    log(event.entity.name)
 --    if event.entity.name == 'extinguisher-remnants' then
         -- you at least know where shots landed
 --    end
 end)
 
-script.on_event(defines.events.on_tick, function (event)
+script.on_event(defines.events.on_tick, function ()
 	for _, player in pairs(game.players) do
 		mine_check(player);
 		evaluate_callbacks();
@@ -102,7 +115,7 @@ end)
 --
 
 local Timeout = {}
-if not dtms.Timeout then dtms.Timeout = Timeout end
+if not global.dtms.Timeout then global.dtms.Timeout = Timeout end
 
 Timeout.callbacks = {} -- pending callback timers
 
